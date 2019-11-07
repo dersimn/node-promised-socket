d7af424b2aa7326410d187320f23e61d71878544
Simon Christmann <simon@christmann.email>
2019-11-07 13:12:48 +0100

    Better don’t use async function here

--- index.js
+++ index.js
@@ -36,36 +36,42 @@ class PromisedSocket extends net.Socket {
             pEvent(this, 'connect').then(() => {
                 resolve();
             }).catch((err) => {
-                reject(err);
+                reject(this._encapsulateError(err));
             });
 
             pEvent(this, 'timeout').then(() => {
                 reject(new Error('Timeout while trying to conenct.'));
             }).catch((err) => {
-                reject(err);
+                reject(this._encapsulateError(err));
             });
 
             super.connect(...args);
         });
     }
-    async writeReadUntil(writeMessage, finishRegex) {
-        this.write(writeMessage);
 
-        try {
+    writeReadUntil(writeMessage, finishRegex) {
+        return new Promise(async (resolve, reject) => {
+            pEvent(this, 'error').then(err => {
+                reject(this._encapsulateError(err));
+            }).catch(err => {
+                reject(new Error('Unkown error.'));
+            });
+
+            pEvent(this, 'timeout').then(() => {
+                reject(new Error('Socket timed out.'));
+            }).catch(err => {
+                reject(new Error('Unkown error.'));
+            });
+
+            this.write(writeMessage);
             let message = '';
             const asyncIterator = pEvent.iterator(this, 'data');
             for await (const event of asyncIterator) {
                 message += event;
                 if (finishRegex.test(event)) break;
             }
-            return message;
-        } catch(err) {
-            if (err instanceof Error) {
-                throw err;
-            } else {
-                throw new Error(err);
-            }
-        }
+            resolve(message);
+        });
     }
 }
 
